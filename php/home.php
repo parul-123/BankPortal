64d3984e6785e039287a4219824cb58973be449f
parul-123 <parulever333@gmail.com>
2016-08-02 13:58:08 +0530

    Create home.php

---
+++ php/home.php
@@ -0,0 +1,101 @@
+<html>
+<head><title>
+	Sabka Bank
+</title>
+<meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="style/home.css" />
+</head>
+
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><img src="images/Logo.png"/></a>
+		</div>
+		<div class="collapse navbar-collapse" id=myNavbar>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a style="color:white;" href="staff1.php">STAFF</a><li>
+				<li><a style="color:white;" href="customers1.php">CUSTOMERS</a><li>
+				<!--<li><a style="color:white;" href="#band">CONTACTS</a><li>-->
+			</ul>
+		</div>
+	</div>
+</nav>
+
+
+<!--<ul>
+  <li style="float:right"><a href="staff1.php">STAFF</a></li>
+  <li style="float:right"><a href="customers1.php">CUSTOMERS</a></li>
+</ul>
+<a href="#"><img src="images/Logo.png"/></a>-->	
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel" >
+	<!--Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+		<!--<li data-target="#myCarousel" data-slide-to="2" class="active"></li>-->
+	</ol>
+	
+	<!Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class ="item active">
+			<img src="images/banking.jpg" alt="Banking" width="1200" height="700">
+		</div>
+		<div class="item">
+			<img src="images/banking2.jpg" alt="Corporate Banking" width="1200" height="700"/>
+		</div>
+	</div>
+	
+	<!--Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  
+  <br>
+  <br>
+  <br>
+  <br>
+  
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Progress of our bank</h2>
+      <h4>With its 2,505 domestic branches & extension counters and 13,146 ATMs across the country, as on 30th September 2014, the network of Sabka Bank spreads across 1,686 cities and towns, enabling the Bank to reach out to a large cross-section of customers with an array of products and services. The Bank also has overseas offices in UK, Singapore, Hong Kong, Shanghai, Colombo, Dubai and Abu Dhabi.</h4> 
+      <p>Sabka Bank encourages its customers to subscribe for e-statements and other electronic formats of communication, thus significantly reducing paper consumption</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-signal logo"></span>
+    </div>
+  </div>
+</div>
+
+ <br>
+ <br>
+ <br>
+ 
+  <div class="container-fluid bg-2 text-center">
+  	<h3> <strong>Security Awareness</strong> </h3>
+  	<p> In case of any suspicious activity on your mobile number, you are requested to change your Internet Banking Login Id and Transaction Password </p>
+  	<p> Sabka Bank will never send you requests asking your passwords or NETSECURE code. Please do not share with anyone. </p>
+  </div>
+  		
+
+</body>
+
+</html>
